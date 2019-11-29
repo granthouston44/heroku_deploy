0b2598a02731a04fdfa22b27d3c4af812a398bd7
Grant Houston <grant.houston44@hotmail.com>
2019-11-29 15:03:54 +0000

    update merchant method

--- models/merchant.rb
+++ models/merchant.rb
@@ -2,45 +2,77 @@ require_relative('../db/sql_runner')
 
 class Merchant
 
-attr_accessor :name
+  attr_accessor :name
 
-#don't change as it is linked to the db
-attr_reader :id
+  #don't change as it is linked to the db
+  attr_reader :id
 
-def initialize(merchant_details)
-  #as the object contains a hash, we want to acces the
-  #right key for the relevant property
-  #if there is an id, get the value and make sure it is an int
-  @id = merchant_details['id'].to_i if merchant_details['id']
-  @name = merchant_details['name']
-end
+  def initialize(merchant_details)
+    #as the object contains a hash, we want to acces the
+    #right key for the relevant property
+    #if there is an id, get the value and make sure it is an int
+    @id = merchant_details['id'].to_i if merchant_details['id']
+    @name = merchant_details['name']
+  end
 
-def self.delete_all()
-  sql = "DELETE FROM merchants;"
-  SqlRunner.run(sql)
-end
+  def self.delete_all()
+    sql = "DELETE FROM merchants;"
+    SqlRunner.run(sql)
+  end
 
-def save()
-sql = "
-INSERT INTO merchants
-(
-  name
-)
-VALUES
-(
-  $1
-)
-RETURNING id
-"
-values = [@name]
-results = SqlRunner.run(sql, values)
-#the returing sql command is bringing back the id
-#the returned id is then assigned to the instance
-#because SQL always returns an array, we want to access
-#the first element of the array, then the key 'id' as
-#as that is what parameters that the Merchant object takes
-@id = results.first()['id'].to_i
-end
+  def save()
+    sql = "
+    INSERT INTO merchants
+    (
+      name
+    )
+    VALUES
+    (
+      $1
+    )
+    RETURNING id
+    "
+    values = [@name]
+    results = SqlRunner.run(sql, values)
+    #the RETURNING sql command is bringing back the id
+    #the returned id is then assigned to the instance
+    #because SQL always returns an array of hashes, we want to access
+    #the first element of the array, then the key 'id' as
+    #as that is what parameters that the Merchant object takes
+    @id = results.first()['id'].to_i
+  end
+
+  def self.all
+    sql = "
+    SELECT * FROM merchants;
+    "
+    result = SqlRunner.run(sql)
+    #SQL runner will not return merchant objects, therefore we want to map
+    #the results as new merchant objects within an array
+    #the merchant object takes in a hash as evidence in our initialize and seeds file
+    #when creating new Merchants.
+    #(result from SqlRunner) will return an array of hashes
+    #in the map, we are telling it for each entry in the array, create a new
+    #merchant object using the hashes contained within the array
+    return result.map {|merchant| Merchant.new(merchant)}
+  end
+
+  def update
+    sql =
+    "
+    UPDATE merchants
+    SET name =
+    $1
+    WHERE id = $2
+    "
+    values = [@name, @id]
+    SqlRunner.run(sql,values)
+  end
+
+  # def self.find(id)
+  #
+  #
+  # end
 
 
 
