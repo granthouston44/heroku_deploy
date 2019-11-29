99774550c1b00c3fa7c70aba196467fb604ef287
Grant Houston <grant.houston44@hotmail.com>
2019-11-29 11:34:31 +0000

    first merchant spec test written

---
+++ specs/merchant_spec.rb
@@ -0,0 +1,16 @@
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../models/merchant')
+
+MiniTest::Reporters.use! MiniTest::Reporters::SpecRerporter.new
+
+class MerchantTest < MiniTest::Test
+
+def test_create_merchant_object
+  Merchant.new({
+    "name" => "Amazon"
+    })
+end
+
+
+end
