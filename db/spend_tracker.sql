c86b1ed1dd620984892cd5828b092e1fa13219a9
Grant Houston <grant.houston44@hotmail.com>
2019-11-29 17:44:50 +0000

    added transactions table to the sql file

--- db/spend_tracker.sql
+++ db/spend_tracker.sql
@@ -1,3 +1,4 @@
+DROP TABLE transactions;
 DROP TABLE merchants;
 DROP TABLE tags;
 
@@ -11,4 +12,13 @@ CREATE TABLE tags
 (
   id SERIAL8 PRIMARY KEY,
   name VARCHAR not null
+);
+
+CREATE TABLE transactions
+(
+  id SERIAL8 PRIMARY KEY,
+  merchant_id INT REFERENCES merchants(id),
+  tag_id INT REFERENCES tags(id),
+  date_of_transaction DATE,
+  amount DECIMAL(4,2)
 );
