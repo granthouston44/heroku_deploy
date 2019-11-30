d2df9e6dc97d1e630edf2aaa9131ced678b30e45
Grant Houston <grant.houston44@hotmail.com>
2019-11-30 12:25:27 +0000

    changed structure, added links to layout

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require('sinatra')
 require('sinatra/contrib/all')
 
+require_relative('./models/transaction')
+require_relative('./models/merchant')
+require_relative('./models/tag')
+also_reload('./models/*')
+
 get '/' do
-  erb(:index)
+  @transactions = Transaction.all
+  erb(:"home")
 end
