c27f70088a52d7c4d1574cd0693c6b74d244ef4c
Grant Houston <grant.houston44@hotmail.com>
2019-11-30 14:23:07 +0000

    view pages tested on sinatra for transactions sorted by merchant and tag

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -7,3 +7,11 @@ require_relative('../models/tag')
 get '/transactions/new' do
   erb(:"transactions/new")
 end
+
+get '/transactions/merchant' do
+  erb(:"transactions/merchant")
+end
+
+get '/transactions/tag' do
+  erb(:"transactions/tag")
+end
