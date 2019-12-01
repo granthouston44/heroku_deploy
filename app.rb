6ceaab2ec1a048c20471c0440fd696045581134b
Grant Houston <grant.houston44@hotmail.com>
2019-12-01 16:27:48 +0000

    added filter selectors for home view, no functionality yet

--- app.rb
+++ app.rb
@@ -1,10 +1,14 @@
 require('sinatra')
 require('sinatra/contrib/all')
 also_reload('./models/*')
+require('pry')
 
 require_relative("./controllers/transactions_controller")
 
 get '/' do
-  @transactions = Transaction.all
+
+  @transactions = Transaction.sort_by_date
+  @tags = Tag.all
+  @merchants = Merchant.all
   erb(:"home")
 end
