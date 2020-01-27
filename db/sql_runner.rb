e1db673f8e8577269028b8c87bc455ffb38eb6b7
Grant Houston <grant.houston44@hotmail.com>
2020-01-27 09:50:57 +0000

    heroku deploy

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -5,8 +5,8 @@ class SqlRunner
   def self.run(sql,values = [])
     begin
       # Connect to the database
-      db = PG.connect({dbname: 'dc6ge8qv4sneas', host: 'ec2-1-1-1-1-1.compute-1.amazonaws.com',
-        port: 5432, user: 'nsdlkdjalskjd', password: 'sadlskjadlkjASDAD'})
+      db = PG.connect({dbname: 'd22g14lifffkdl', host: 'ec2-54-174-221-35.compute-1.amazonaws.com',
+        port: 5432, user: 'dxecgbdeiexhsx', password: '53f9f79fcd091acca765fae7837790f830b01155035130200fec8a94f7d61ab1'})
       # Prepare the SQL command
       db.prepare("query", sql)
       # Execute the SQL and get the properties within the values array
